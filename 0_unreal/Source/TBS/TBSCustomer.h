31ccefb33ef265e604e56add3f2b665cbd79f729
Jack_Harb <steven.bartels@icloud.com>
2015-04-14 23:38:14 +0200

    + added CSV Struct for Prototyp BearLoad

--- 0_unreal/Source/TBS/TBSCustomer.h
+++ 0_unreal/Source/TBS/TBSCustomer.h
@@ -3,7 +3,33 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
 #include "TBSCustomer.generated.h"
+
+USTRUCT(BlueprintType)
+struct FBeardMeshData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	FBeardMeshData()
+		: TranslateX(0)
+		, TranslateY(0)
+		, TranslateZ(0)
+		, RotateX(0)
+		, RotateY(0)
+		, RotateZ(0)
+	{}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Translation")	float TranslateX;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Translation")	float TranslateY;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Translation")	float TranslateZ;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")	float RotateX;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")	float RotateY;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")	float RotateZ;
+};
 
 UCLASS()
 class TBS_API ATBSCustomer : public AActor
