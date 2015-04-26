8df6c0bbed457f9c520f4d0cc629594f0f1a6502
Vincent Mahnke <accounts@vimaster.de>
2015-04-26 23:24:01 +0200

    ~ Transferred Radio from Blueprint to actual code

---
+++ 0_unreal/Source/TBS/TBSRadio.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TBS.h"
+#include "TBSRadio.h"
+
+
+// Sets default values
+ATBSRadio::ATBSRadio()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	// Create a camera boom...
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->AttachTo(RootComponent);
+
+	Music0 = CreateDefaultSubobject<UAudioComponent>(TEXT("Music0"));
+	Music0->AttachTo(Mesh);
+	Music0->bOverrideAttenuation = true;
+	Music0->bAllowSpatialization = true;
+
+	Music0->AttenuationOverrides.bAttenuate = true;
+	Music0->AttenuationOverrides.bSpatialize = true;
+	Music0->AttenuationOverrides.AttenuationShape = EAttenuationShape::Cone;
+	Music0->AttenuationOverrides.AttenuationShapeExtents.X = 510;
+	Music0->AttenuationOverrides.AttenuationShapeExtents.Y = 30;
+	Music0->AttenuationOverrides.FalloffDistance = 225;
+}
+
+// Called when the game starts or when spawned
+void ATBSRadio::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Music0->Play();
+}
+
+// Called every frame
+void ATBSRadio::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ 0_unreal/Source/TBS/TBSRadio.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "TBSRadio.generated.h"
+
+UCLASS()
+class TBS_API ATBSRadio : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true")) class UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Audio, meta = (AllowPrivateAccess = "true")) class UAudioComponent* Music0;
+
+	// Sets default values for this actor's properties
+	ATBSRadio();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
