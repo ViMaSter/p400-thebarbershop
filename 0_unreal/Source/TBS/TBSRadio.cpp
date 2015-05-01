c7a8052bb03b075c9058d0c72a41141f67da0c6e
Vincent Mahnke <accounts@vimaster.de>
2015-05-01 02:33:55 +0200

    + Rough radio-channel prototype

--- 0_unreal/Source/TBS/TBSRadio.cpp
+++ 0_unreal/Source/TBS/TBSRadio.cpp
@@ -7,7 +7,7 @@
 // Sets default values
 ATBSRadio::ATBSRadio () {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 
@@ -15,33 +15,88 @@ ATBSRadio::ATBSRadio () {
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 	Mesh->AttachTo (RootComponent);
 
-	Music0 = CreateDefaultSubobject<UAudioComponent>(TEXT("Music0"));
-	Music0->AttachTo (Mesh);
+	AudioComponentOffset = CreateDefaultSubobject<USceneComponent> (TEXT ("AudioComponentOffset"));
+	AudioComponentOffset->AttachTo (RootComponent);
+	AudioComponentOffset->RelativeLocation = FVector (0, 0, 37.75);
+	AudioComponentOffset->RelativeRotation = FRotator (0, -90.0, 0);
+
+	Music0 = CreateDefaultSubobject<UAudioComponent> (TEXT ("Music0"));
+	Music0->AttachTo (AudioComponentOffset);
 	Music0->bOverrideAttenuation = true;
 	Music0->bAllowSpatialization = true;
+	Music0->OnAudioFinished.AddDynamic (this, &ATBSRadio::AudioFinished0);
 
-	Music0->AttenuationOverrides.bAttenuate = true;
-	Music0->AttenuationOverrides.bSpatialize = true;
-	Music0->AttenuationOverrides.AttenuationShape = EAttenuationShape::Cone;
-	Music0->AttenuationOverrides.AttenuationShapeExtents.X = 510;
-	Music0->AttenuationOverrides.AttenuationShapeExtents.Y = 30;
-	Music0->AttenuationOverrides.FalloffDistance = 225;
+	Music1 = CreateDefaultSubobject<UAudioComponent> (TEXT ("Music1"));
+	Music1->AttachTo (AudioComponentOffset);
+	Music1->bOverrideAttenuation = true;
+	Music1->bAllowSpatialization = true;
+	Music1->OnAudioFinished.AddDynamic (this, &ATBSRadio::AudioFinished1);
+
+	ChannelSwitchNoise = CreateDefaultSubobject<UAudioComponent> (TEXT ("ChannelSwitchNoise"));
+	ChannelSwitchNoise->AttachTo (AudioComponentOffset);
+	ChannelSwitchNoise->bOverrideAttenuation = true;
+	ChannelSwitchNoise->bAllowSpatialization = true;
+
+	NewIs1 = true;
+	CurrentStation = 0;
 
 	PlayMusic = true;
 }
 
 // Called when the game starts or when spawned
 void ATBSRadio::BeginPlay () {
-	Super::BeginPlay();
+	Super::BeginPlay ();
 
 	if (PlayMusic) {
-		Music0->Play();
+		Music0->Play ();
 	}
 }
 
 // Called every frame
 void ATBSRadio::Tick (float DeltaTime) {
 	Super::Tick(DeltaTime);
-
 }
 
+void ATBSRadio::AudioFinished0 () {
+	if (ChannelToFadeIn == Music0) {
+		Music0->Sound = RadioStations[CurrentStation].NextTrack ();
+		Music0->Play (0.0f);
+	}
+}
+
+void ATBSRadio::AudioFinished1 () {
+	if (ChannelToFadeIn == Music1) {
+		Music1->Sound = RadioStations[CurrentStation].NextTrack ();
+		Music1->Play (0.0f);
+	}
+}
+
+void ATBSRadio::SwitchStation (uint8 direction) {
+	CurrentStation += direction;
+	while (CurrentStation < 0) {
+		CurrentStation += RadioStations.Num ();
+	}
+
+	CurrentStation %= RadioStations.Num ();
+
+	UAudioComponent* CurrentComponent = NewIs1 ? Music0 : Music1;
+	UAudioComponent* NewComponent = NewIs1 ? Music1 : Music0;
+	NewComponent->Sound = RadioStations[CurrentStation].NextTrack ();
+
+	ChannelToFadeIn = NewComponent;
+
+	CurrentComponent->FadeOut (0.5f, 0.0f);
+	ChannelSwitchNoise->Play ();
+	ChannelToFadeIn->Stop ();
+	ChannelToFadeIn->FadeIn (
+		0.5f,
+		1.0f,
+		FMath::Fmod (GetWorld ()->TimeSeconds, ChannelToFadeIn->Sound->Duration)
+		);
+	NewIs1 = !NewIs1;
+}
+
+void ATBSRadio::SwitchStationFadeIn () {
+	GetWorldTimerManager ().ClearTimer (ChannelFadeIn);
+	
+}
--- 0_unreal/Source/TBS/TBSRadio.h
+++ 0_unreal/Source/TBS/TBSRadio.h
@@ -5,27 +5,76 @@
 #include "GameFramework/Actor.h"
 #include "TBSRadio.generated.h"
 
+USTRUCT (BlueprintType)
+struct FTBSRadioStation {
+	GENERATED_USTRUCT_BODY ()
+
+public:
+	FTBSRadioStation () : CurrentTrack(0) {}
+	uint32 CurrentTrack;
+
+	UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = Audio)
+	TArray<USoundBase*> MusicPieces;
+
+	USoundBase* NextTrack () {
+		USoundBase* Track = MusicPieces[CurrentTrack];
+
+		CurrentTrack += 1;
+		CurrentTrack %= MusicPieces.Num ();
+
+		return Track;
+	}
+};
+
 UCLASS()
 class TBS_API ATBSRadio : public AActor {
 	GENERATED_BODY ()
 
 public:
+	// Component
 	UPROPERTY (VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* Mesh;
 
-	UPROPERTY (VisibleAnywhere, BlueprintReadOnly, Category = Audio, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY (VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	USceneComponent* AudioComponentOffset;
+
+	UPROPERTY (VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	UAudioComponent* Music0;
+
+	UPROPERTY (VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UAudioComponent* Music1;
+
+	UPROPERTY (VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UAudioComponent* ChannelSwitchNoise;
+	UAudioComponent* ChannelToFadeIn;
+	FTimerHandle ChannelFadeIn;
+
+	UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = Audio, meta = (AllowPrivateAccess = "true"))
+	TArray<FTBSRadioStation> RadioStations;
+	bool NewIs1;
+	int CurrentStation;
 
 	UPROPERTY (EditAnywhere, BlueprintReadWrite, Category = Audio)
 	bool PlayMusic;
-
 
 	// Sets default values for this actor's properties
 	ATBSRadio ();
 
 	// Called when the game starts or when spawned
+	UFUNCTION (BlueprintCallable, Category = Radio)
+	void SwitchStation (uint8 direction);
+	void SwitchStationFadeIn ();
+
+	UFUNCTION (Category = Radio)
+	void AudioFinished0 ();
+	UFUNCTION (Category = Radio)
+	void AudioFinished1 ();
+
+	// Called when the game starts or when spawned
 	virtual void BeginPlay () override;
 
+	// Tick helper
+	void ApplyFrequency (float DeltaTime);
 	// Called every frame
 	virtual void Tick (float DeltaSeconds) override;
 
