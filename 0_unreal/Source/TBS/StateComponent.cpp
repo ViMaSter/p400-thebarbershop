a00f15a8e6300ef1161f99dbf4ad3212f2e62384
Vincent Mahnke <accounts@vimaster.de>
2015-05-10 22:30:18 +0200

    ~ Introduction of StateComponent with BP-support

---
+++ 0_unreal/Source/TBS/StateComponent.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TBS.h"
+#include "StateComponent.h"
+
+
+// Sets default values for this component's properties
+UStateComponent::UStateComponent() {
+	bWantsInitializeComponent = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UStateComponent::InitializeComponent() {
+	Super::InitializeComponent();
+
+	// ...
+
+}
+
+void UStateComponent::ResizeStateArray(uint8 newSize) {
+	while (RawStates.Num() < newSize) {
+		RawStates.Add(false);
+	}
+}
+
+bool UStateComponent::GetState(uint8 offset) {
+	if (RawStates.Num() < (offset + 1)) {
+		ResizeStateArray(offset + 1);
+	}
+
+	return RawStates[offset];
+}
+
+bool UStateComponent::SetState(uint8 offset, bool value) {
+	if (RawStates.Num() < (offset + 1)) {
+		ResizeStateArray(offset + 1);
+	}
+
+	RawStates[offset] = value;
+	return true;
+}
---
+++ 0_unreal/Source/TBS/StateComponent.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "StateComponent.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class TBS_API UStateComponent : public UActorComponent {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UStateComponent();
+
+	// Called when the game starts
+	virtual void InitializeComponent() override;
+
+	// Called every frame
+	// virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+
+	TArray<bool> RawStates;
+	void ResizeStateArray(uint8 newSize);
+
+	UFUNCTION(BlueprintCallable, Category = "State") bool GetState(uint8 offset);
+	UFUNCTION(BlueprintCallable, Category = "State") bool SetState(uint8 offset, bool value);
+};
