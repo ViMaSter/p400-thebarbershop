93d3bebc21417237b102fa1d4c10774ba83b141a
Vincent Mahnke <accounts@vimaster.de>
2015-05-12 19:08:05 +0200

    + Added 9-patch preparation tool

---
+++ 5_tools/9patch_generator/0_vs/9patch_generator/9patch_generator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csv_concatenator
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading .PNG-files...");
+
+            if (args.Length < 1)
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Multiselect = true;
+                openFileDialog1.Filter = ".PNG-files (*.png)|*.png";
+                DialogResult dialogResult = openFileDialog1.ShowDialog();
+                if (dialogResult == DialogResult.OK) // Test result.
+                {
+                    args = openFileDialog1.FileNames;
+                }
+            }
+
+            foreach (string pathToFile in args)
+            {
+                Bitmap src = new Bitmap(pathToFile);
+                Bitmap target = new Bitmap(src.Width - 2, src.Height - 2);
+
+                for (int y = 0; y < target.Height; y++)
+                {
+                    for (int x = 0; x < target.Width; x++)
+                    {
+                        target.SetPixel(x, y, src.GetPixel(x+1, y+1));
+                    }
+                }
+
+                target.Save(pathToFile.Replace(".png", ".9patch.png"));
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("FERTIG!");
+            Console.WriteLine("Eingabetaste zum Schliessen druecken!");
+            Console.Read();
+        }
+    }
+}
