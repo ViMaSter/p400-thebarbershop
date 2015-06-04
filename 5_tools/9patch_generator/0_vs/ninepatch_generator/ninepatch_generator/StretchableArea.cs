fd15a04da3afc357fa759e8b3178334c53cba613
Vincent Mahnke <accounts@vimaster.de>
2015-06-04 22:21:52 +0200

    + Added ToString() support ~ Accessibility interface polish

--- 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/StretchableArea.cs
+++ 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/StretchableArea.cs
@@ -8,14 +8,38 @@ namespace ninepatch_generator
 {
     class StretchableArea
     {
-        int Start = 0;
-        int Length = 0;
-        bool IsHorizontal = false;
+        private int start = 0;
+        private int length = 0;
+
+        public int Start
+        {
+            get {
+                return start;
+            }
+        } 
+        public int Length {
+            get {
+                return length;
+            }
+        }
+
+        public bool IsHorizontal = false;
+
+        #region String converter
+        static public implicit operator string(StretchableArea lhs) {
+            return lhs.ToString();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[%d, %d]", Start, Length);
+        }
+        #endregion
 
         public StretchableArea(int start, int length, bool isHorizontal)
         {
-            Start = start;
-            Length = length;
+            this.start = start;
+            this.length = length;
             IsHorizontal = isHorizontal;
         }
     }
