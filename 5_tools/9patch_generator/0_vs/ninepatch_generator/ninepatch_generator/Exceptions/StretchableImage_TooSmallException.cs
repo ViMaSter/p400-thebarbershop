c29c701008d469204bca48cca9982fd68c804402
Vincent Mahnke <accounts@vimaster.de>
2015-06-10 22:21:01 +0200

    ~ StretchableImage_TooSmallException now requires the smallest allowed size in the constructor

--- 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/Exceptions/StretchableImage_TooSmallException.cs
+++ 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/Exceptions/StretchableImage_TooSmallException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,25 @@ namespace ninepatch_generator.Exceptions
 {
     class StretchableImage_TooSmallException : Exception
     {
+        public static string InfoText = "Smallest allowed resolution: {0}x{1}px";
+        public Size SmallestAllowedSize;
+        public StretchableImage_TooSmallException(Size smallestAllowedSize)
+        {
+            SmallestAllowedSize = smallestAllowedSize;
+        }
 
+        /// <seealso cref="ToString()">
+        static public implicit operator string(StretchableImage_TooSmallException lhs)
+        {
+            return lhs.ToString();
+        }
+        /// <summary>
+        /// Generates a user-readable debug-output of this object
+        /// </summary>
+        /// <returns>Human-readable info about this exception</returns>
+        public override string ToString()
+        {
+            return string.Format(InfoText, SmallestAllowedSize.Width, SmallestAllowedSize.Height);
+        }
     }
 }
