43ab94cd4218e686cfbcce59c94e70761edea4d8
Vincent Mahnke <accounts@vimaster.de>
2015-06-11 17:09:01 +0200

    ~ First complete working interface

--- 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/Source/9patch_generator.Designer.cs
+++ 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/Source/9patch_generator.Designer.cs
@@ -28,18 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.numericHeight = new System.Windows.Forms.NumericUpDown();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.numericWidth = new System.Windows.Forms.NumericUpDown();
+            this.numericWidthTooSmall_error = new System.Windows.Forms.ErrorProvider(this.components);
+            this.numericHeightTooSmall_error = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericWidthTooSmall_error)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericHeightTooSmall_error)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBoxPreview
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1239, 657);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxPreview.BackgroundImage = global::ninepatch_generator.Properties.Resources.background;
+            this.pictureBoxPreview.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxPreview.Location = new System.Drawing.Point(13, 13);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(1000, 657);
+            this.pictureBoxPreview.TabIndex = 0;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(1019, 53);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(38, 13);
+            this.labelWidth.TabIndex = 2;
+            this.labelWidth.Text = "Width:";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(1019, 79);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(41, 13);
+            this.labelHeight.TabIndex = 4;
+            this.labelHeight.Text = "Height:";
+            // 
+            // numericHeight
+            // 
+            this.numericHeight.Enabled = false;
+            this.numericHeight.Location = new System.Drawing.Point(1132, 77);
+            this.numericHeight.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            0});
+            this.numericHeight.Name = "numericHeight";
+            this.numericHeight.Size = new System.Drawing.Size(120, 20);
+            this.numericHeight.TabIndex = 3;
+            this.numericHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericHeight.ValueChanged += new System.EventHandler(this.numericHeight_ValueChanged);
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(1022, 13);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(230, 23);
+            this.buttonImport.TabIndex = 5;
+            this.buttonImport.Text = "Import image";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(1022, 119);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(230, 23);
+            this.buttonExport.TabIndex = 6;
+            this.buttonExport.Text = "Export image";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // numericWidth
+            // 
+            this.numericWidth.Enabled = false;
+            this.numericWidth.Location = new System.Drawing.Point(1132, 51);
+            this.numericWidth.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            0});
+            this.numericWidth.Name = "numericWidth";
+            this.numericWidth.Size = new System.Drawing.Size(120, 20);
+            this.numericWidth.TabIndex = 1;
+            this.numericWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericWidth.ValueChanged += new System.EventHandler(this.numericWidth_ValueChanged);
+            // 
+            // numericWidthTooSmall_error
+            // 
+            this.numericWidthTooSmall_error.BlinkRate = 1000;
+            this.numericWidthTooSmall_error.ContainerControl = this;
+            this.numericWidthTooSmall_error.RightToLeft = true;
+            // 
+            // numericHeightTooSmall_error
+            // 
+            this.numericHeightTooSmall_error.BlinkRate = 1000;
+            this.numericHeightTooSmall_error.ContainerControl = this;
+            this.numericHeightTooSmall_error.RightToLeft = true;
             // 
             // ninepatch_generator
             // 
@@ -47,19 +141,40 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 682);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.numericHeight);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.numericWidth);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "ninepatch_generator";
             this.Text = "9 patch generator";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.ninepatch_generator_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.ninepatch_generator_DragEnter);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericWidthTooSmall_error)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericHeightTooSmall_error)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.NumericUpDown numericHeight;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.NumericUpDown numericWidth;
+        private System.Windows.Forms.ErrorProvider numericWidthTooSmall_error;
+        private System.Windows.Forms.ErrorProvider numericHeightTooSmall_error;
 
     }
 }
--- 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/Source/9patch_generator.cs
+++ 5_tools/9patch_generator/0_vs/ninepatch_generator/ninepatch_generator/Source/9patch_generator.cs
@@ -12,6 +12,20 @@ namespace ninepatch_generator
 {
     public partial class ninepatch_generator : Form
     {
+        StretchableImage CurrentImage;
+        string Filename;
+        Size DestinationSize
+        {
+            get
+            {            
+                return new Size((int)numericWidth.Value, (int)numericHeight.Value);
+            }
+            set
+            {
+                numericHeight.Value = value.Height;
+                numericWidth.Value = value.Width;
+            }
+        }
         public ninepatch_generator()
         {
             InitializeComponent();
@@ -26,16 +40,58 @@ namespace ninepatch_generator
                 {
                     if (File.Exists(fileLoc))
                     {
-                        StretchableImage img;
-                        img = new StretchableImage(new System.Drawing.Bitmap(fileLoc));
-                        MessageBox.Show(img);
-                        if (img.IsValid())
-                        {
-                            Image image = img.GenerateImage(new Size(423+400, 414+200));
-                            pictureBox1.Image = image;
-                        }
+                        LoadImage(Path.GetFileNameWithoutExtension(fileLoc), new System.Drawing.Bitmap(fileLoc));
                     }
                 }
+            }
+        }
+
+        private void LoadImage(string filename, Bitmap bitmap, bool supressWarning = false)
+        {
+            // "Overwrite current file?"-warning
+            if (CurrentImage != null && !supressWarning)
+            {
+                if (MessageBox.Show("Do you want to discard the currently loaded image?", "Overwrite current file", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            // Validity check
+            StretchableImage NewImage = new StretchableImage(bitmap);
+            if (!NewImage.IsValid())
+            {
+                DialogResult res;
+                res = MessageBox.Show("The image you provided is not a valid 9-patch image!", "Error loading file", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+
+                if (res == DialogResult.Retry)
+                {
+                    BrowseForFile(true);
+                }
+
+                return;
+            }
+
+            numericWidth.Enabled = true;
+            numericHeight.Enabled = true;
+
+            // Applying and initial rendering
+            CurrentImage = NewImage;
+            Filename = filename;
+            DestinationSize = CurrentImage.StaticAreaTotal + CurrentImage.DynamicAreaTotal;
+            pictureBoxPreview.Image = CurrentImage.GenerateImage(DestinationSize);
+        }
+
+        private void BrowseForFile(bool supressWarning = false)
+        {
+            // Prepare file dialog
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Supported image files (*.BMP, *.GIF, *.JPG, *.PNG, *.TIFF, *.EXIF)|*.BMP;*.GIF;*.JPG;*.PNG;*.TIFF;*.EXIF";
+            ofd.Title = "Load 9-patch image...";
+
+            // Open image dialog
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                LoadImage(Path.GetFileNameWithoutExtension(ofd.FileName), new System.Drawing.Bitmap(ofd.FileName), supressWarning);
             }
         }
 
@@ -43,6 +99,93 @@ namespace ninepatch_generator
         {
             e.Effect = DragDropEffects.Copy;
             Console.WriteLine(e.Data);
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            BrowseForFile();
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = ".png";
+            sfd.Filter = "PNG file|*.PNG|BMP file|*.BMP|EMF file|*.EMF|EXIF file|*.EXIF|GIF file|*.GIF|ICON file|*.ICO|JPEG file|*.JPG|TIFF file|*.TIFF|WMF file|*.WMF";
+            sfd.FileName = string.Format("{0}_{1}x{2}", Filename, DestinationSize.Width, DestinationSize.Height);
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                System.Drawing.Imaging.ImageFormat desiredImageFormat;
+                switch (sfd.FilterIndex) {
+                    case 1:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Png;
+                        break;
+                    case 2:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Bmp;
+                        break;
+                    case 3:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Emf;
+                        break;
+                    case 4:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Exif;
+                        break;
+                    case 5:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Gif;
+                        break;
+                    case 6:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Icon;
+                        break;
+                    case 7:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                        break;
+                    case 8:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Tiff;
+                        break;
+                    case 9:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Wmf;
+                        break;
+                    default:
+                        desiredImageFormat = System.Drawing.Imaging.ImageFormat.Png;
+                        break;
+                }
+            
+                CurrentImage.GenerateImage(DestinationSize).Save(sfd.FileName, desiredImageFormat);
+            }
+        }
+
+        private void numericWidth_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBoxPreview.Image = CurrentImage.GenerateImage(DestinationSize);
+                numericWidthTooSmall_error.Clear();
+                buttonExport.Enabled = true;
+            }
+            catch (Exceptions.StretchableImage_TooSmallException ex)
+            {
+                numericWidthTooSmall_error.SetError(numericWidth, "Value too small! Minimum required width: " + CurrentImage.StaticAreaTotal.Width);
+                buttonExport.Enabled = false;
+            }
+            catch (Exceptions.StretchableImage_NullException ex)
+            {
+            }
+        }
+
+        private void numericHeight_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBoxPreview.Image = CurrentImage.GenerateImage(DestinationSize);
+                numericHeightTooSmall_error.Clear();
+                buttonExport.Enabled = true;
+            }
+            catch (Exceptions.StretchableImage_TooSmallException ex)
+            {
+                numericHeightTooSmall_error.SetError(numericHeight, "Value too small! Minimum required height: " + CurrentImage.StaticAreaTotal.Height);
+                buttonExport.Enabled = false;
+            }
+            catch (Exceptions.StretchableImage_NullException ex)
+            {
+            }
         }        
 
     }
